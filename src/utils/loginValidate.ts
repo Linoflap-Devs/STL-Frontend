85f62923ed5fc22f4d091c02f94c2844d5565cb3
staneunbl <shayyannelee@gmail.com>
2025-03-04 09:49:14 +0800

    frontend adjustment on backend breaking changes on login

--- src/utils/loginValidate.ts
+++ src/utils/loginValidate.ts
@@ -1,15 +1,14 @@
 // src\utils\loginValidate.ts
-
 import axios from 'axios';
 
 // validation
-export const verifyCredentials = (username: string, password: string): Record<string, string> => {
+export const verifyCredentials = (email: string, password: string): Record<string, string> => {
   const errors: Record<string, string> = {};
   
   const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
   const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*])[A-Za-z\d!@#$%^&*]{8,}$/;
 
-  if (!username.trim()) {
+  if (!email.trim()) {
     errors.username = 'Username is required.';
   }
   if (!password.trim()) {
