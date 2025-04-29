9ea46458d373dddec0e4852a33051764cfb9aa23
ivanvargass <165545622+IvanVargas21@users.noreply.github.com>
2025-04-30 01:09:06 +0800

    Added: the TableOperatorsSummary Component as well as the required button

--- src/pages/Protected/operators/index.tsx
+++ src/pages/Protected/operators/index.tsx
@@ -1,6 +1,8 @@
 import { Box, Typography} from "@mui/material"
 import CardsOperatorsPage from "~/components/operators/OperatorsCards"
 import ChartOperatorsSummary from "~/components/operators/OperatorsSummary"
+import TableOperatorsSummary from "~/components/operators/OperatorsSummaryTable"
+
 const OperatorPage = () => {
     return (
       <Box>
@@ -23,6 +25,26 @@ const OperatorPage = () => {
             }}
           >
             <ChartOperatorsSummary/>
+            <TableOperatorsSummary/>
+              <Box
+                sx={{ display: "flex", justifyContent: "flex-end" }}
+                >
+                  <button
+                    style={{
+                      backgroundColor: "#67ABEB",
+                      color: "#000",
+                      border: "none",
+                      padding: "10px 24px",
+                      borderRadius: "8px",
+                      fontSize: "12px",
+                      fontWeight: "400",
+                      cursor: "pointer",
+                      width: "150px",
+                    }}
+                  >
+                    Export as CSV
+                  </button>
+              </Box>
           </Box>
       </Box>
     )
