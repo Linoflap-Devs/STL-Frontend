7f02f9c1229bfa3a6fb18b1525feb07b04021fa6
ivanvargass <165545622+IvanVargas21@users.noreply.github.com>
2025-04-30 00:04:33 +0800

    Added: Initial Operators Page

--- src/pages/Protected/operators/index.tsx
+++ src/pages/Protected/operators/index.tsx
@@ -1,5 +1,31 @@
+import { Box, Typography} from "@mui/material"
+import CardsOperatorsPage from "~/components/operators/OperatorsCards"
+import ChartOperatorsSummary from "~/components/operators/OperatorsSummary"
 const OperatorPage = () => {
-    return ("Hello world")
+    return (
+      <Box>
+          <Typography sx={{ fontWeight: 700 }} variant="h4">
+            Small Town Lottery Operators
+          </Typography>
+
+          <Box>
+            <CardsOperatorsPage/>
+          </Box>
+
+          <Box 
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              gap: 2,
+              height: "100%",
+              width: "100%",
+              mt: 2,
+            }}
+          >
+            <ChartOperatorsSummary/>
+          </Box>
+      </Box>
+    )
 }
 
 export default OperatorPage
