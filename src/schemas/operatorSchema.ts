74252bf45e262b43f0a4b17b89f318984754452e
ivanvargass <165545622+IvanVargas21@users.noreply.github.com>
2025-05-02 19:42:29 +0800

    Added: Operator Schema using Zod Validation

---
+++ src/schemas/operatorSchema.ts
@@ -0,0 +1,46 @@
+import { z } from 'zod';
+
+export const operatorSchema = z.object({
+  companyName: z.string().min(1, 'Company Name is required'),
+  email: z.string().email('Invalid email').min(1, 'Email is required'),
+  phone: z.string().min(1, 'Phone Number is required'),
+  dateOfOperations: z.string().min(1, 'Date of Operations is required'),
+  areaOfOperations: z.string().min(1, 'Area of Operations is required'),
+  gameTypes: z.object({
+    stlPares: z.boolean(),
+    stlSwer2: z.boolean(),
+    stlSwer3: z.boolean(),
+    stlSwer4: z.boolean(),
+    allGames: z.boolean(),
+  }),
+});
+
+export const updateOperatorSchema = z.object({
+  status: z.string(),
+  companyName: z.string().min(1, 'Company Name is required'),
+  email: z.string().email('Invalid email').min(1, 'Email is required'),
+  phone: z.string().min(1, 'Phone Number is required'),
+  dateOfOperations: z.string().min(1, 'Date of Operations is required'),
+  areaOfOperations: z.string().min(1, 'Area of Operations is required'),
+  gameTypes: z.object({
+    stlPares: z.boolean(),
+    stlSwer2: z.boolean(),
+    stlSwer3: z.boolean(),
+    stlSwer4: z.boolean(),
+    allGames: z.boolean(),
+  }),
+  createdBy: z.string().min(1, 'Complete name is required'),
+  latestUpdateBy: z.string().min(1, 'Complete name is required'),
+  creationDate: z.string().refine((val) => {
+    const regex = /^\d{4}\/\d{2}\/\d{2} \d{2}:\d{2}:\d{2}$/;
+    return regex.test(val);
+  }, {
+    message: "Invalid datetime format (expected YYYY/MM/DD HH:mm:ss)",
+  }),
+  latesUpdateDate:  z.string().refine((val) => {
+    const regex = /^\d{4}\/\d{2}\/\d{2} \d{2}:\d{2}:\d{2}$/;
+    return regex.test(val);
+  }, {
+    message: "Invalid datetime format (expected YYYY/MM/DD HH:mm:ss)",
+  })
+})
